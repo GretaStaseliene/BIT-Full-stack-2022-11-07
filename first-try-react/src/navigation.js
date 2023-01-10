6d346a15e27fc6e0d276f9f3f536ff5647cd28ec
GretaStaseliene <gretatamasauskait3@gmail.com>
2023-01-10 17:57:25 +0200

    homework

---
+++ first-try-react/src/navigation.js
@@ -0,0 +1,24 @@
+// function navigation() {
+//     return (
+//         <nav class="navbar navbar-expand-lg bg-light">
+//             <div class="container-fluid">
+//                 <a class="navbar-brand" href="#">React App</a>
+//                 <div class="collapse navbar-collapse" id="navbarNav">
+//                     <ul class="navbar-nav">
+//                         <li class="nav-item">
+//                             <a class="nav-link active" aria-current="page" href="#">Home</a>
+//                         </li>
+//                         <li class="nav-item">
+//                             <a class="nav-link" href="#">Features</a>
+//                         </li>
+//                         <li class="nav-item">
+//                             <a class="nav-link" href="#">Pricing</a>
+//                         </li>
+//                     </ul>
+//                 </div>
+//             </div>
+//         </nav>
+//     );
+// }
+
+// export default navigation;
