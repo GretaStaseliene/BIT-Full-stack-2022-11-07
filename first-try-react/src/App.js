5fd8bbe87c3f7dd8e4fd8648994bba3d4c82e247
GretaStaseliene <gretatamasauskait3@gmail.com>
2023-01-16 21:08:35 +0200

    To Do list, edit funcionallity needs to be fixed

--- first-try-react/src/App.js
+++ first-try-react/src/App.js
@@ -1,24 +1,113 @@
-import logo from './logo.svg';
 import './App.css';
+import { useId, useState } from 'react';
 
-function App() {
+const Header = ({ nav }) => {
+  const id = useId();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Norėdami sužinoti daugiau apie React biblioteką - spauskite nuorodą žemiau
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <h1>UAB "Šaunu"</h1>
+      <nav>
+        <ul>
+          { nav.map((value, index) => <li key={id + index}>{value}</li>) }
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+const Container = (props) => {
+  return (
+    <div className='container'>{props.children}</div>
+  );
+}
+
+// const FormEntry = () => {
+//   const [value, setValue] = useState('Iveskite teksta i laukeli');
+
+//   const keistiReiksme = (event) => {
+//     setValue(event.target.value);
+//   }
+
+//   return (
+//     <>
+//       <input type="text" onChange={keistiReiksme} />
+//       {/* <input type="text" onChange={(e) => setValue(event.target.value)} /> */}
+//       <div className='rezultatas'>{value}</div>
+//     </>
+//   );
+// }
+
+const FormEntry = () => {
+  const [tasks, setTasks] = useState([]);
+  const [currentTask, setCurrentTask] = useState();
+
+  const handleForm = (e) => {
+    e.preventDefault();
+
+    setTasks([...tasks, { name: currentTask, done: false }]);
+  }
+
+  const handleDone = (e, index) => {
+    tasks[index].done = true;
+
+    setTasks([...tasks]);
+  }
+
+  const handleEdit = (e, index) => {
+    let newTasks = [...tasks];
+    let task = newTasks[index];
+
+    let newTask = prompt(`Update ${task.name}?`);
+    let taskObj = { todo: newTask, complete: false };
+    // newTasks.splice(index, 1, taskObj);
+
+    if(newTask === null || newTask === '') {
+      return;
+    } else {
+      task.todo = newTask;
+    }
+
+    setTasks([...newTasks]);
+  }
+
+  const handleDelete = (e, index) => {
+    tasks.splice(index, 1);
+    console.log(tasks);
+    setTasks([...tasks]);
+  }
+
+  return (
+    <>
+      <h2 className='mb-2'>Task Manager</h2>
+      <form className='input-group' onSubmit={handleForm}>
+        <input type='text' className='form-control' onChange={(e) => setCurrentTask(e.target.value)} />
+        <button className='btn btn-primary'>Issaugoti</button>
+      </form>
+      {tasks.map((value, index) =>
+        <li 
+        key={value.name + index} 
+        className={value.done ? 'done' : ''}>{value.name}
+        <button className='btn btn-success me-2 ms-2 mt-2' onClick={ (e) => handleDone(e, index) }>Atlikta</button>
+        <button className='btn btn-info text-light me-2 mt-2' onClick={ (e) => handleEdit(e, index) }>Redaguoti</button>
+        <button className='btn btn-danger mt-2' onClick={ (e) => handleDelete(e, index) }>Ištrinti</button>
+        </li>
+      )}
+    </>
+  );
+}
+
+const App = () => {
+  const navigacija = ['Titulinis', 'Apie mus', 'Kontaktai'];
+
+  return (
+    <Container>
+      <Header nav={navigacija} />
+      <main>
+        <FormEntry />
+      </main>
+    </Container>
+
   );
 }
 
